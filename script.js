c2135e1210f7d04edd6e08e4e7c9091c4fcc38a7
Aziza0011 <khakimovaaziza95@gmail.com>
2024-07-20 10:03:01 +0500

    first commit

--- script.js
+++ script.js
@@ -3,12 +3,32 @@ let list = document.querySelector('ol')
 
 btn.addEventListener('click', ()=>{
     let inp =document.querySelector('input').value
+    if(inp === ''){
+        alert('write something')
+    }else{
+        let newli= document.createElement('li')
+        list.appendChild(newli)
+        newli.textContent=inp
 
-    let newli= document.createElement('li')
-    list.appendChild(newli)
-    newli.textContent=inp
-    const btn = () => { alert("Hello, World!"); };
+
+        let newspan = document.createElement('span')
+        newspan.innerHTML ="\u00d7";
+        newli.appendChild(newspan)
+    }
+    
+    
+})
+
+let ul = document.querySelector('ol')
+
+ul.addEventListener('click', (e)=> {
+    if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove()
+    }else if(e.target.tagName === 'LI'){
+        newli.style.color = 'red'
+       }
 })
 
 
+    
 
